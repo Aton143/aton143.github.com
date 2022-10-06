0fcc0ef4ec304d78cb0aae69c85895a12ae38434
Antonio Martinez <antoniomrtinez@gmail.com>
2022-10-05 17:44:24 -0700

    Added draggable elements

---
+++ js/toy.js
@@ -0,0 +1,69 @@
+"use strict";
+
+function makePoint(x, y) {
+  return {x: x, y: y};
+}
+
+// NOTE(antonio): first - second 
+function firstMinusSecondPoint(first, second) {
+  return makePoint(first.x - second.x, first.y - second.y);
+}
+
+function makeOnMouseDownDraggableElementEventListener(element, elementDictionary) {
+  return function(event) {
+    elementDictionary[element.id].start = makePoint(event.clientX, event.clientY);
+
+    const elementMouseMoveEventListener = makeOnMouseMoveDraggableElementEventListener(element, elementDictionary);
+    elementDictionary[element.id].mouseMoveEventListener = elementMouseMoveEventListener;
+    document.addEventListener("mousemove", elementMouseMoveEventListener);
+
+    const elementMouseUpEventListener = makeOnMouseUpDraggableElementEventListener(element, elementDictionary);
+    elementDictionary[element.id].mouseUpEventListener = elementMouseUpEventListener;
+    document.addEventListener("mouseup", elementMouseUpEventListener);
+  }
+}
+
+function makeOnMouseMoveDraggableElementEventListener(element, elementDictionary) {
+  return function(event) {
+    event.preventDefault();
+
+    const currentMousePosition = makePoint(event.clientX, event.clientY);
+
+    const startingMousePosition = elementDictionary[element.id].start;
+    const endingMousePosition = firstMinusSecondPoint(startingMousePosition, currentMousePosition);
+
+    elementDictionary[element.id].end = endingMousePosition;
+    elementDictionary[element.id].start = currentMousePosition;
+
+    const startElementPosition = makePoint(element.offsetLeft, element.offsetTop) 
+    const calculatedElementPosition = firstMinusSecondPoint(startElementPosition, endingMousePosition);
+
+    element.style.left = `${calculatedElementPosition.x}px`;
+    element.style.top = `${calculatedElementPosition.y}px`;
+  }
+}
+
+function makeOnMouseUpDraggableElementEventListener(element, elementDictionary) {
+  return function(event) {
+    document.removeEventListener("mousemove", elementDictionary[element.id].mouseMoveEventListener);
+    document.removeEventListener("mouseup", elementDictionary[element.id].mouseUpEventListener);
+
+    elementDictionary[element.id].mouseMoveEventListener = null;
+    elementDictionary[element.id].mouseUpEventListener = null;
+  }
+}
+
+window.onload = function(event) {
+  // NOTE(antonio): the only requirement that the element has is that it must have a unique id
+  const draggableElementsDictionary = {};
+
+  const draggableElements = document.getElementsByClassName("my-draggable");
+  for (let element of draggableElements) {
+    draggableElementsDictionary[element.id] = {
+      start: makePoint(0, 0),
+      end: makePoint(0, 0),
+    };
+
+    element.addEventListener("mousedown", makeOnMouseDownDraggableElementEventListener(element, draggableElementsDictionary));
+  }
+}
