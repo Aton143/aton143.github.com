8792d22470739a1b79051f50af7f2d79076ecf83
Antonio Martinez <antoniomrtinez@gmail.com>
2022-10-06 19:16:12 -0700

    Draggable list but buggy

--- js/toy.js
+++ js/toy.js
@@ -1,7 +1,36 @@
 "use strict";
 
+window.onload = function(event) {
+  const draggableElementsDictionary = {};
+
+  const draggableElements = document.getElementById("my-draggable-list").children;
+  let draggableElementIndex = 0;
+
+  for (let element of draggableElements) {
+    element.id = `draggable-${draggableElementIndex}`;
+
+    draggableElementsDictionary[element.id] = {
+      element: element,
+      start: makePoint(0, 0),
+      end: makePoint(0, 0),
+      originalPosition: makePoint(0, 0),
+      mouseMoveEventListener: null,
+      elementMouseUpEventListener: null,
+      index: draggableElementIndex,
+    };
+
+    element.addEventListener("mousedown", makeOnMouseDownDraggableElementEventListener(element, draggableElementsDictionary));
+
+    draggableElementIndex++;
+  }
+}
+
 function makePoint(x, y) {
   return {x: x, y: y};
+}
+
+function addPoints(first, second) {
+  return makePoint(first.x + second.x, first.y + second.y);
 }
 
 // NOTE(antonio): first - second 
@@ -16,6 +45,7 @@ function makeOnMouseDownDraggableElementEventListener(element, elementDictionary
     if ((elementDictionary[element.id].mouseMoveEventListener !== null) &&
         (elementDictionary[element.id].mouseUpEventListener !== null)) {
       elementDictionary[element.id].mouseUpEventListener(null);
+      element.style.cursor = "grab";
     } else {
       const elementMouseMoveEventListener = makeOnMouseMoveDraggableElementEventListener(element, elementDictionary);
       elementDictionary[element.id].mouseMoveEventListener = elementMouseMoveEventListener;
@@ -24,6 +54,14 @@ function makeOnMouseDownDraggableElementEventListener(element, elementDictionary
       const elementMouseUpEventListener = makeOnMouseUpDraggableElementEventListener(element, elementDictionary);
       elementDictionary[element.id].mouseUpEventListener = elementMouseUpEventListener;
       document.addEventListener("mouseup", elementMouseUpEventListener);
+
+      element.style.position = "relative";
+      element.style.cursor = "grabbing";
+
+      const mouseDownDraggableElementClienRect = element.getBoundingClientRect();
+      elementDictionary[element.id].originalPosition =
+        makePoint(mouseDownDraggableElementClienRect.left, mouseDownDraggableElementClienRect.top);
+      console.log(elementDictionary[element.id].originalPosition);
     }
   }
 }
@@ -32,19 +70,31 @@ function makeOnMouseMoveDraggableElementEventListener(element, elementDictionary
   return function(event) {
     event.preventDefault();
 
+    // NOTE(antonio): need to check this
     const currentMousePosition = makePoint(event.clientX, event.clientY);
+    const calculatedElementPosition = firstMinusSecondPoint(currentMousePosition, elementDictionary[element.id].originalPosition);
 
-    const startingMousePosition = elementDictionary[element.id].start;
-    const endingMousePosition = firstMinusSecondPoint(startingMousePosition, currentMousePosition);
-
-    elementDictionary[element.id].end = endingMousePosition;
-    elementDictionary[element.id].start = currentMousePosition;
-
-    const startElementPosition = makePoint(element.offsetLeft, element.offsetTop) 
-    const calculatedElementPosition = firstMinusSecondPoint(startElementPosition, endingMousePosition);
-
-    element.style.left = `${calculatedElementPosition.x}px`;
+    element.style.left = `${calculatedElementPosition.x }px`;
     element.style.top = `${calculatedElementPosition.y}px`;
+
+    const mouseMoveDraggableElementIndex = elementDictionary[element.id].index;
+    const mouseMoveDraggableElementClienRect = element.getBoundingClientRect();
+
+    for (let elementKey in elementDictionary) {
+      const currentElementIndex = elementDictionary[elementKey].index;
+      const currentElementClientRect = elementDictionary[elementKey].element.getBoundingClientRect();
+
+      if ((currentElementIndex < mouseMoveDraggableElementIndex) &&
+          (mouseMoveDraggableElementClienRect.top < currentElementClientRect.top)) {
+        elementDictionary[element.id].index = currentElementIndex;
+        elementDictionary[elementKey].index = mouseMoveDraggableElementIndex;
+      }
+      if ((currentElementIndex > mouseMoveDraggableElementIndex) &&
+          (mouseMoveDraggableElementClienRect.top > currentElementClientRect.top)) {
+        elementDictionary[element.id].index = currentElementIndex;
+        elementDictionary[elementKey].index = mouseMoveDraggableElementIndex;
+      }
+    }
   }
 }
 
@@ -55,22 +105,29 @@ function makeOnMouseUpDraggableElementEventListener(element, elementDictionary) 
 
     elementDictionary[element.id].mouseMoveEventListener = null;
     elementDictionary[element.id].mouseUpEventListener = null;
-  }
-}
 
-window.onload = function(event) {
-  // NOTE(antonio): the only requirement that the element has is that it must have a unique id
-  const draggableElementsDictionary = {};
+    element.style.cursor = "grab";
+    
+    const elementIndexArray = [];
+    for (let elementKey in elementDictionary) {
+      elementIndexArray.push({
+        index: elementDictionary[elementKey].index,
+        element: elementDictionary[elementKey].element
+      });
+    }
 
-  const draggableElements = document.getElementsByClassName("my-draggable");
-  for (let element of draggableElements) {
-    draggableElementsDictionary[element.id] = {
-      start: makePoint(0, 0),
-      end: makePoint(0, 0),
-      mouseMoveEventListener: null,
-      elementMouseUpEventListener: null,
-    };
+    elementIndexArray.sort((a, b) => {
+      return a.index > b.index;
+    });
 
-    element.addEventListener("mousedown", makeOnMouseDownDraggableElementEventListener(element, draggableElementsDictionary));
+    const listElement = element.parentNode;
+    while (listElement.firstChild) {
+      listElement.removeChild(listElement.firstChild);
+    }
+
+    for (let elementIndex of elementIndexArray) {
+      listElement.appendChild(elementIndex.element);
+      element.style.position = "static";
+    }
   }
 }
